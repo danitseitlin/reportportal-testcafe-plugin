e9263085996766955c6de21c69782f3ff174ec54
Dani Tseitlin <dani.tseitlin@gmail.com>
2020-12-20 02:32:14 +0200

    Migrating to API file to TS

--- src/api.ts
+++ src/api.ts
@@ -1,18 +1,23 @@
 /* eslint-disable no-undefined */
+import Axios, { AxiosError, AxiosInstance, AxiosResponse } from 'axios';
+import fs from 'fs';
+//const axios = require('axios');
+//const fs = require('fs');
 
-const axios = require('axios');
-const fs = require('fs');
+export class API {
+    options: Options
+    baseURL: string
+    token: string
+    headers: {[key: string]: any}
+    client: AxiosInstance
 
-class API {
-
-    constructor (options) {
-        this.options = options;
+    constructor (options: Options) {
         this.baseURL = `${options.protocol}://${options.domain}${options.apiPath}`;
         this.token = options.token;
         this.headers = { 'Content-type': 'application/json', 'Authorization': `Bearer ${options.token}` };
-        this.client = axios.create({
+        this.client = Axios.create({
             baseURL: this.baseURL,
-            headers: { 'Content-type': 'application/json', 'Authorization': `Bearer ${options.token}` }
+            headers: this.headers
         });
     }
 
@@ -33,7 +38,7 @@ class API {
      * @param {*} projectName The name of the project
      * @param {*} options The options of the launch
      */
-    async createLaunch (projectName, options) {
+    async createLaunch (projectName: string, options: CreateLaunchParameters) {
         try {
             return this.handleResponse(await this.client.post(`/${projectName}/launch`, options));
         }
@@ -48,7 +53,7 @@ class API {
      * @param {*} launchId The id of the launch
      * @param {*} options The options of the launch
      */
-    async finishLaunch (projectName, launchId, options) {
+    async finishLaunch (projectName: string, launchId: string, options: FinishLaunchParameters) {
         try {
             return this.handleResponse(await this.client.put(`/${projectName}/launch/${launchId}/finish`, options));
         }
@@ -63,7 +68,7 @@ class API {
      * @param {*} launchId The id of the launch
      * @param {*} options The options of the launch
      */
-    async forceStopLaunch (projectName, launchId, options) {
+    async forceStopLaunch (projectName: string, launchId: string, options: FinishLaunchParameters) {
         try {
             return this.handleResponse(await this.client.put(`/${projectName}/launch/${launchId}/stop`, options));
         }
@@ -77,7 +82,7 @@ class API {
      * @param {*} projectName The name of the project 
      * @param {*} options The options of the launch
      */
-    async createTestItem (projectName, options) {
+    async createTestItem (projectName: string, options: CreateTestItemParameters) {
         try {
             return this.handleResponse(await this.client.post(`/${projectName}/item`, options));
         }
@@ -89,12 +94,12 @@ class API {
     /**
      * Creating a child test item
      * @param {*} projectName The name of the project
-     * @param {*} parentItem The parent item of the test item
+     * @param {*} parentItemId The parent item of the test item
      * @param {*} options The options of the child test item
      */
-    async createChildTestItem (projectName, parentItem, options) {
+    async createChildTestItem (projectName: string, parentItemId: string, options: CreateTestItemParameters) {
         try {
-            return this.handleResponse(await this.client.post(`/${projectName}/item/${parentItem}`, options));
+            return this.handleResponse(await this.client.post(`/${projectName}/item/${parentItemId}`, options));
         }
         catch (error) {
             return this.handleError(error);
@@ -107,7 +112,7 @@ class API {
      * @param {*} testItemId The id of the test item
      * @param {*} options The options of the test item
      */
-    async finishTestItem (projectName, testItemId, options) {
+    async finishTestItem (projectName: string, testItemId: string, options: FinishTestItemParameters) {
         try {
             return this.handleResponse(await this.client.put(`/${projectName}/item/${testItemId}`, options));
         }
@@ -122,7 +127,7 @@ class API {
      * @param {*} filePart The file
      * @param {*} boundary The boundary
      */
-    buildMultiPartStream (jsonPart, filePart, boundary) {
+    buildMultiPartStream (jsonPart: CreateLogParameters[], filePart: {name: string, type: string, content: any }, boundary: string) {
         const eol = '\r\n';
         const bx = `--${boundary}`;
         const buffers = [
@@ -151,13 +156,13 @@ class API {
      * @param {*} projectName The name of the project
      * @param {*} options The options of the log item
      */
-    async sendLog (projectName, options) {
+    async sendLog (projectName: string, options: CreateLogParameters) {
         try {
             options.message = this.isJSON(options.message) || Array.isArray(options.message) ? JSON.stringify(options.message) : options.message;
             if (options.file) {
                 const MULTIPART_BOUNDARY = Math.floor(Math.random() * 10000000000).toString();
                 const fullPath = options.file.path;
-                const instance = axios.create({
+                const instance = Axios.create({
                     baseURL: this.baseURL,
                     headers: { 'Content-type': `multipart/form-data; boundary=${MULTIPART_BOUNDARY}`, 'Authorization': `Bearer ${this.token}` }
                 });
@@ -179,7 +184,7 @@ class API {
      * Checking if item is a valid JSON
      * @param {*} json The string of the JSON
      */
-    isJSON (json) {
+    isJSON (json: string): boolean {
         try {
             JSON.parse(json);
             return true;
@@ -192,7 +197,7 @@ class API {
     /**
      * Retrieving the timestamp right now
      */
-    now () {
+    now (): number {
         return new Date().valueOf();
     }
 
@@ -200,7 +205,7 @@ class API {
      * Handling an Axios response
      * @param {*} response The object of the response
      */
-    handleResponse (response) {
+    handleResponse (response: AxiosResponse) {
         return response.data;
     }
     
@@ -208,7 +213,7 @@ class API {
      * Handling an Axios error
      * @param {*} error The error response
      */
-    handleError (error) {
+    handleError (error: AxiosError) {
         const errorMessage = error.message;
         const responseData = error.response && error.response.data;
 
@@ -220,4 +225,121 @@ class API {
     }
 }
 
-module.exports = API;
+/**
+ * The contructor options
+ */
+export type Options = { protocol: 'https' | 'http', domain: string, apiPath: string, token: string }
+
+/**
+ * The launch creation parameters
+ */
+export type CreateLaunchParameters = {
+    name:        string,
+    startTime:   number,
+    description?: string,
+    rerun?: boolean,
+    rerunOf?: boolean,
+    mode?: 'DEFAULT' | 'DEBUG',
+    attributes?: ItemAttribute[],
+    [key: string]: any
+}
+
+/**
+ * The launch finish parameters
+ */
+export type FinishLaunchParameters = {
+    attributes?: ItemAttribute[],
+    description?: string,
+    endTime: number,
+    status?: TestItemStatus
+}
+
+/**
+ * The item attribute object
+ */
+export type ItemAttribute = {
+    key: string,
+    system?: boolean,
+    value: string 
+}
+
+/**
+ * The item parameter object
+ */
+export type ItemParameter = {
+    key: string,
+    value?: string
+}
+
+/**
+ * The test item creation parameters
+ */
+export type CreateTestItemParameters = {
+    attributes?: ItemAttribute[],
+    codeRef?: string,
+    description?: string,
+    hasStats?: boolean,
+    launchUuid: string,
+    name: string,
+    parameters: ItemParameter[],
+    retry?: boolean,
+    retryOf?: string,
+    startTime?: string,
+    testCaseId: string,
+    type: TestItemType,
+    uniqueId?: string
+}
+
+/**
+ * The test item finish parameters
+ */
+export type FinishTestItemParameters = {
+    attributes?: ItemAttribute[],
+    endTime: number,
+    issue: {
+        autoAnalyzed?: boolean,
+        comment?: string,
+        externalSystemIssues: {
+            btsProject?: string,
+            btsUrl?: string,
+            submitData?: number,
+            ticketId?: string,
+            url?: string
+        },
+        ignoreAnalyzer?: boolean,
+        issueType: string
+    },
+    launchUuid: string,
+    retry?: boolean,
+    retryOf?: boolean,
+    testCaseId?: string,
+    status?: TestItemStatus
+}
+
+/**
+ * The log creation parameters
+ */
+export type CreateLogParameters = {
+    file: { name: string, [key: string]: any },
+    itemUuid?: string,
+    launchUuid: string,
+    level?: LogLevel,
+    message?: string,
+    time: string,
+    uuid?: string
+}
+
+/**
+ * The available test item types
+ */
+export type TestItemType = 'SUITE' | 'STORY' | 'TEST' | 'SCENARIO' | 'STEP' | 'BEFORE_CLASS' | 'BEFORE_GROUPS' | 'BEFORE_METHOD' | 'BEFORE_SUITE' | 'BEFORE_TEST' | 'AFTER_CLASS' | 'AFTER_GROUPS' | 'AFTER_METHOD' | 'AFTER_SUITE' | 'AFTER_TEST';
+
+/**
+ * The available log levels
+ */
+export type LogLevel = 'error' | 'warn' | 'info' | 'debug' | 'trace' | 'fatal' | 'unknown';
+
+/**
+ * The available test item statuses
+ */
+export type TestItemStatus = 'PASSED'| 'FAILED'| 'STOPPED'| 'SKIPPED'| 'INTERRUPTED'| 'CANCELLED'| 'INFO'| 'WARN';
