a76ba7c549cdf361cd4b94712ba99f853bbfccb3
Dani Tseitlin <dani.tseitlin@gmail.com>
2021-03-04 21:47:54 +0200

    Adding ability to integrate E2E automation for TestCafe and Report Portal

---
+++ tests/test.unit.ts
@@ -0,0 +1,35 @@
+
+import { MockServer } from 'dmock-server';
+import { mock } from './mock';
+import createTestCafe  from 'testcafe'
+let reportPortalServer: MockServer;
+let testcafeServer: TestCafe;
+
+describe('Mock server testing', async function() {
+    this.timeout(10 * 60 * 60);
+    before(async () => {
+        reportPortalServer = new MockServer({
+            hostname: 'localhost',
+            port: 1234,
+            routes: mock
+        });
+        reportPortalServer.start()
+        testcafeServer = await createTestCafe('localhost', 1337, 1338);
+    });
+
+    after(async () => {
+        if(testcafeServer)
+            await testcafeServer.close();
+        reportPortalServer.stop()
+    });
+
+    it('Running TestCafe Tests', async () => {
+        const runner = testcafeServer.createRunner();
+        const failedCount = await runner
+        .src(['tests/test.testcafe.ts'])
+        .browsers(['firefox'])
+        .run();
+
+        console.log('Tests failed: ' + failedCount);
+    });
+});
