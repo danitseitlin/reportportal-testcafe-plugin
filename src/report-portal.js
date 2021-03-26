475c0a9847b2b628baf706ee861df6d9bd050629
Dani Tseitlin <dani.tseitlin@gmail.com>
2021-03-26 13:43:23 +0300

    .

--- src/report-portal.js
+++ src/report-portal.js
@@ -147,8 +147,8 @@ class ReportPortal {
      */
     async sendTestLogs (testId, level, message, time = this.client.now(), attachment, retry = 3) {
         try {
-            if(this.client.displayDebugLogs)
-                process.stdout.write(`\n [Test ${testId}] Sending log: ${message} \n`)
+            //if(this.client.displayDebugLogs)
+            //    process.stdout.write(`\n [Test ${testId}] Sending log: ${message} \n`)
             await this.client.sendLog(this.projectName, {
                 itemUuid:   testId,
                 launchUuid: this.launch.id,
