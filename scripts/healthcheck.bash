bbc46aa7545765302a1bb55ae5bd368c5e5ee61c
Dani Tseitlin <dani.tseitlin@gmail.com>
2024-09-28 03:37:43 +0300

    .

--- scripts/healthcheck.bash
+++ scripts/healthcheck.bash
@@ -1,12 +1,26 @@
 #!/bin/bash
-set -eux
+#set -eux
+#
+#wait-for-status() {
+#    echo "Waiting for services status"
+#    echo ""
+#    bash -c \
+#    'while [[ "$(npm run status)" == *"(starting)"* ]];\
+#    do echo "Waiting for services" && sleep 30;\
+#    done'
+#}
+#wait-for-status
 
-wait-for-status() {
-    echo "Waiting for services status"
-    echo ""
-    bash -c \
-    'while [[ "$(npm run status)" == *"(starting)"* ]];\
-    do echo "Waiting for services" && sleep 30;\
-    done'
-}
-wait-for-status
+while true; do
+  # Run the command and capture the output
+  output=$(npm run start)
+
+  # Check if the output contains 'starting' or 'exited'
+  if echo "$output" | grep -q -e "starting" -e "exited"; then
+    echo "Detected 'starting' or 'exited' in the output. Sleeping for 2 seconds..."
+    sleep 2
+  else
+    echo "No 'starting' or 'exited' found in the output. Continuing..."
+    break
+  fi
+done
